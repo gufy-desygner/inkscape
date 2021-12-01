b124291c7dc1f8f53d5757955df2b5ebeab75ff0
Sergey Stepanov <serg_admin@rambler.ru>
2021-12-01 15:21:41 +0300

    Some optimization

--- src/extension/internal/pdfinput/svg-builder.cpp
+++ src/extension/internal/pdfinput/svg-builder.cpp
@@ -3920,55 +3920,64 @@ std::vector<SvgTextPosition> SvgBuilder::getTextInArea(double x1, double y1, dou
         int start = -1;
         int end = -1;
 
+        if (isSimulate)
+        {
+        	Geom::Point p(textX1, textY1);
+        	if (sqCellBBox.interiorContains(p))
+        	{
+        		textInsideCell = uniTextPosition;
+        	}
+        } else {
         // Check every Letter position in the text if inside a Cell!
-        for(int i = 0; i < data_x.size(); i++) {
-            if (data_x[i]._set) {
-                    // Now you can start extracting characters!
-                    Geom::Point p(textX1 + data_x[i].value, textY1);
-                    if (sqCellBBox.interiorContains(p)) {
-                    	if (start == -1) start = i;
-                    	end = i;
-                        //printf("Point inside Cell Found!\n");
-                        bIsPointInsideCellFound = true;
-                        Inkscape::CSSOStringStream os_x;
-                        textInsideCell += uniTextPosition[i];
-                    }
-            }
+			for(int i = 0; i < data_x.size(); i++) {
+				if (data_x[i]._set) {
+						// Now you can start extracting characters!
+						Geom::Point p(textX1 + data_x[i].value, textY1);
+						if (sqCellBBox.interiorContains(p)) {
+							if (start == -1) start = i;
+							end = i;
+							//printf("Point inside Cell Found!\n");
+							bIsPointInsideCellFound = true;
+							Inkscape::CSSOStringStream os_x;
+							textInsideCell += uniTextPosition[i];
+						}
+				}
+			}
         }
         if (!textInsideCell.empty()){
 			Inkscape::XML::Node* tspanAfterStart = nullptr;
 			if (! isSimulate) // isSimulate - avoid generate new nodes
-			if (start > 0 || ((end + 1) != uniTextPosition.size() && (end+1) > 0))
-			{
+				if (start > 0 || ((end + 1) != uniTextPosition.size() && (end+1) > 0))
+				{
 
-				if (start > 0)
-				{
-					tspanAfterStart = splitTspan(start, textPosition.ptextNode);
-					//textPosition.ptextNode = tspanAfterStart;
-				} else {
-					tspanAfterStart = textPosition.ptextNode;
-				}
-				if ((end + 1) != uniTextPosition.size() && (end +1) > 0)
-				{
-					if (start == 0)
+					if (start > 0)
 					{
-						textPosition.needRemove = true;
-						tspanAfterStart = textPosition.ptextNode->duplicate(textPosition.ptextNode->document());
+						tspanAfterStart = splitTspan(start, textPosition.ptextNode);
+						//textPosition.ptextNode = tspanAfterStart;
+					} else {
+						tspanAfterStart = textPosition.ptextNode;
+					}
+					if ((end + 1) != uniTextPosition.size() && (end +1) > 0)
+					{
+						if (start == 0)
+						{
+							textPosition.needRemove = true;
+							tspanAfterStart = textPosition.ptextNode->duplicate(textPosition.ptextNode->document());
+						}
+
+						//std::string content(tspanAfterStart->firstChild()->content());
+
+						trimSvgArrayRight(end, "data-x", tspanAfterStart);
+						trimSvgArrayRight(end, "dx", tspanAfterStart);
+
+						tspanAfterStart->firstChild()->setContent(textInsideCell.c_str());
 					}
 
-					//std::string content(tspanAfterStart->firstChild()->content());
-
-					trimSvgArrayRight(end, "data-x", tspanAfterStart);
-					trimSvgArrayRight(end, "dx", tspanAfterStart);
-
-					tspanAfterStart->firstChild()->setContent(textInsideCell.c_str());
+					if (tspanAfterStart != textPosition.ptextNode && textPosition.ptextNode->parent())
+					{
+						textPosition.ptextNode->parent()->addChild(tspanAfterStart, textPosition.ptextNode);
+					}
 				}
-
-				if (tspanAfterStart != textPosition.ptextNode && textPosition.ptextNode->parent())
-				{
-					textPosition.ptextNode->parent()->addChild(tspanAfterStart, textPosition.ptextNode);
-				}
-			}
 
 
             //printf("[Cell Text Found : %s]\n", textInsideCell.c_str());
